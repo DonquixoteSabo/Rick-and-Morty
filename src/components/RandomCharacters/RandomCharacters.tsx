731a4b667db73e34ed9f093552da08c76422ecc9
Donquixote Sabo <mixboksu@gmail.com>
2021-08-10 17:03:54 +0200

    feat: :sparkles: Fetch data from database

--- src/components/RandomCharacters/RandomCharacters.tsx
+++ src/components/RandomCharacters/RandomCharacters.tsx
@@ -1,6 +1,20 @@
 // import { Wrapper, Button } from './Categories.styles';
+import { getRandomNumbers } from 'helpers/getRandomNumbers';
+import { useQuery } from 'react-query';
+
+const getCharacters = async (nums: number[]) => {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/${nums}`
+  );
+  return response.json();
+};
 
 function RandomCharacters() {
+  const data = useQuery('characterAmount', () =>
+    getCharacters(getRandomNumbers())
+  );
+  console.log(data);
+
   return (
     <>
       <h1>random</h1>
