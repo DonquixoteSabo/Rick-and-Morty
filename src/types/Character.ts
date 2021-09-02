95c7589de892543c968448646cab889377c76c90
Donquixote Sabo <mixboksu@gmail.com>
2021-09-02 06:59:52 +0200

    feat: :pencil2: Update Types

--- src/types/Character.ts
+++ src/types/Character.ts
@@ -1,9 +1,13 @@
+import { Status } from './Status';
+
 export interface Character {
   id: string;
   name: string;
-  status: 'Alive' | 'Dead' | 'unknown';
+  status: Status;
   species: string;
   location: { name: string; link: string };
   origin: { name: string };
   image: string;
+  gender: string;
+  type: string;
 }
